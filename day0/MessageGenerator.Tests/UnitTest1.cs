77e1847d13714c9969ec38b4a7ed727d262d5714
Juan Martinez <jmartinez@flinks.com>
2022-12-01 07:56:15 -0500

    Day 0 - Empty project

---
+++ day0/MessageGenerator.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace AOC2022.Day0.MessageGenerator.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ day0/MessageGenerator/Class1.cs
@@ -0,0 +1,5 @@
+﻿namespace AOC2022.Day0.MessageGenerator;
+public class Class1
+{
+
+}
