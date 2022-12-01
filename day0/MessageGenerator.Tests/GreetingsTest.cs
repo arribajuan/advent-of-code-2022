ae0ef34e520bf788da2f5bf390f3ec3a5be2dc71
Juan Martinez <jmartinez@flinks.com>
2022-12-01 08:50:48 -0500

    Day 0 - add unit tests

---
+++ day0/MessageGenerator.Tests/GreetingsTest.cs
@@ -0,0 +1,13 @@
+namespace AOC2022.Day0.MessageGenerator.Tests;
+
+public class GreetingsTest
+{
+    [Fact]
+    public void HelloTest()
+    {
+        string expectedResult = "Hello World!";
+
+        Greetings greetings = new Greetings();
+        Assert.True(greetings.GenerateHello() == expectedResult, $"Expected '{expectedResult}'");
+    }
+}
--- day0/MessageGenerator.Tests/Usings.cs
+++ day0/MessageGenerator.Tests/Usings.cs
@@ -1,1 +1,3 @@
 global using Xunit;
+
+using AOC2022.Day0.MessageGenerator;
