f8fec642484e4969be7020bc678c3c3261b15295
Juan Martinez <jmartinez@flinks.com>
2022-12-01 17:27:58 -0500

    Day 1 - add unit tests for calorie counting

--- day1/CalorieCounter.Tests/ElfCalorieCounterTest.cs
+++ day1/CalorieCounter.Tests/ElfCalorieCounterTest.cs
@@ -4,29 +4,22 @@ namespace AOC2022.Day1.CalorieCounter.Tests;
 
 public class ElfCalorieCounterTest
 {
-    private ElfCalorieCounter ecc;
-    private string sampleDataPath = string.Empty;
-    private List<string> sampleData;
-    
-
-    public ElfCalorieCounterTest()
-    {
-        this.ecc = new ElfCalorieCounter();
-
-        this.sampleDataPath = Environment.CurrentDirectory + "/assets/data/sample-data.txt";
-        this.sampleData = ecc.LoadInput(this.sampleDataPath);
-    }
-
     [Fact]
     public void TextFileIsLoaded()
     {
-        Assert.NotEmpty(this.sampleData);
+        ElfCalorieCounter ecc = new ElfCalorieCounter();
+        List<string> sampleData = GetSampleData();
+
+        Assert.NotEmpty(sampleData);
     }
 
     [Fact]
     public void TextFileHas14Rows()
     {
-        Assert.Equal(14, this.sampleData.Count);
+        ElfCalorieCounter ecc = new ElfCalorieCounter();
+        List<string> sampleData = GetSampleData();
+
+        Assert.Equal(14, sampleData.Count);
     }
 
     [Theory]
@@ -36,7 +29,42 @@ public class ElfCalorieCounterTest
     [InlineData(13, "10000")]
     public void TextFileHasSpecificData(int inputPosition, string expectedValue)
     {
-        Assert.True(expectedValue == this.sampleData[inputPosition], $"Expected '{expectedValue}' at position {inputPosition}, got '{this.sampleData[inputPosition]}'");
+        ElfCalorieCounter ecc = new ElfCalorieCounter();
+        List<string> sampleData = GetSampleData();
+
+        Assert.True(expectedValue == sampleData[inputPosition], $"Expected '{expectedValue}' at position {inputPosition}, got '{sampleData[inputPosition]}'");
     }
 
+    [Theory]
+    [InlineData(0, 6000)]
+    [InlineData(1, 4000)]
+    [InlineData(2, 11000)]
+    [InlineData(3, 24000)]
+    [InlineData(4, 10000)]
+    public void CountCaloriesPerElfYieldsSpecificCalorieTotals(int elfPosition, int expectedTotalCalories)
+    {
+        List<int> calorieTotals = GetCalorieTotals();
+
+        Assert.True(expectedTotalCalories == calorieTotals[elfPosition], $"Expected '{expectedTotalCalories}' at position {elfPosition}, got '{calorieTotals[elfPosition]}'");
+    }
+
+    private List<string> GetSampleData()
+    {
+        string sampleDataPath = Environment.CurrentDirectory + "/assets/data/sample-data.txt";
+
+        ElfCalorieCounter ecc = new ElfCalorieCounter();
+        List<string> sampleData = ecc.LoadInput(sampleDataPath);
+
+        return sampleData;
+    }
+
+    private List<int> GetCalorieTotals()
+    {
+        List<string> sampleData = GetSampleData();
+
+        ElfCalorieCounter ecc = new ElfCalorieCounter();
+        List<int> calorieTotals = ecc.CountCaloriesPerElf(sampleData);
+
+        return calorieTotals;
+    }
 }
