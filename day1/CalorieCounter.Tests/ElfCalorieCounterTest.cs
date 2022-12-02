70183f8bdbe35670b29d3ea4d45cbcb4b17f40c9
Juan Martinez <jmartinez@flinks.com>
2022-12-01 19:14:31 -0500

    Day 1 - Change to use AppDomain base directory
    This ensures that we are able to locate the data files in terminal and visual studio (we get the same resulting directory)

--- day1/CalorieCounter.Tests/ElfCalorieCounterTest.cs
+++ day1/CalorieCounter.Tests/ElfCalorieCounterTest.cs
@@ -50,7 +50,7 @@ public class ElfCalorieCounterTest
 
     private List<string> GetSampleData()
     {
-        string sampleDataPath = Environment.CurrentDirectory + "/sample-data.txt";
+        string sampleDataPath = AppDomain.CurrentDomain.BaseDirectory + "sample-data.txt";
 
         ElfCalorieCounter ecc = new ElfCalorieCounter();
         List<string> sampleData = ecc.LoadInput(sampleDataPath);
