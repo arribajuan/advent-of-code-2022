9a998e4b31a6e4f5d12dddeb9ef5700e455efd05
Juan Martinez <jmartinez@flinks.com>
2022-12-02 19:50:00 -0500

    Day 2 - implementation

--- day2/RPS/ElfStrategyGuide.cs
+++ day2/RPS/ElfStrategyGuide.cs
@@ -6,9 +6,45 @@ namespace AOC2022.Day2.RPS
 	{
         public static List<Round> Decrypt(List<string> encryptedGuide)
         {
-            return new List<Round>();
-        }
+            List<Round> decryptedGuide = new List<Round>();
 
+            foreach(string encryptedRound in encryptedGuide)
+            {
+                Round decryptedRound = new Round();
+
+                string encryptedOpponentHand = encryptedRound.Substring(0, 1);
+                switch (encryptedOpponentHand)
+                {
+                    case "A":
+                        decryptedRound.OpponentHand = Enumerations.Hand.Rock;
+                        break;
+                    case "B":
+                        decryptedRound.OpponentHand = Enumerations.Hand.Paper;
+                        break;
+                    case "C":
+                        decryptedRound.OpponentHand = Enumerations.Hand.Scissor;
+                        break;
+                }
+
+                string encryptedPlayerHand = encryptedRound.Substring(2, 1);
+                switch (encryptedPlayerHand)
+                {
+                    case "X":
+                        decryptedRound.PlayerHand = Enumerations.Hand.Rock;
+                        break;
+                    case "Y":
+                        decryptedRound.PlayerHand = Enumerations.Hand.Paper;
+                        break;
+                    case "Z":
+                        decryptedRound.PlayerHand = Enumerations.Hand.Scissor;
+                        break;
+                }
+
+                decryptedGuide.Add(decryptedRound);
+            }
+
+            return decryptedGuide;
+        }
     }
 }
 
--- day2/RPS/Tournament.cs
+++ day2/RPS/Tournament.cs
@@ -4,11 +4,6 @@ namespace AOC2022.Day2.RPS
 {
 	public class Tournament
     {
-		public Tournament()
-		{
-			
-		}
-
 		public static TournamentResult PlayTournament(List<Round> roundsToPlay)
 		{
 			return new TournamentResult();
@@ -16,12 +11,74 @@ namespace AOC2022.Day2.RPS
 
         public static RoundResult PlayRound(Round roundToPlay)
 		{
-			return new RoundResult();
+			RoundResult result = new RoundResult();
+
+            #region Calculate result based points
+
+            // Opponent wins
+            if ((roundToPlay.OpponentHand == Enumerations.Hand.Rock && roundToPlay.PlayerHand == Enumerations.Hand.Scissor) ||
+                 (roundToPlay.OpponentHand == Enumerations.Hand.Paper && roundToPlay.PlayerHand == Enumerations.Hand.Rock) ||
+                 (roundToPlay.OpponentHand == Enumerations.Hand.Scissor && roundToPlay.PlayerHand == Enumerations.Hand.Paper)
+                )
+            {
+                result.OpponentPointsThisRound = 6;
+                result.PlayerPointsThisRound = 0;
+                result.PlayerOutcome = Enumerations.Outcome.Lose;
+            }
+
+            // Player wins
+            if ((roundToPlay.PlayerHand == Enumerations.Hand.Rock && roundToPlay.OpponentHand == Enumerations.Hand.Scissor) ||
+                 (roundToPlay.PlayerHand == Enumerations.Hand.Paper && roundToPlay.OpponentHand == Enumerations.Hand.Rock) ||
+                 (roundToPlay.PlayerHand == Enumerations.Hand.Scissor && roundToPlay.OpponentHand == Enumerations.Hand.Paper)
+                )
+            {
+                result.OpponentPointsThisRound = 0;
+                result.PlayerPointsThisRound = 6;
+                result.PlayerOutcome = Enumerations.Outcome.Win;
+            }
+
+            // Tie
+            if (roundToPlay.OpponentHand == roundToPlay.PlayerHand)
+			{
+				result.OpponentPointsThisRound = 3;
+				result.PlayerPointsThisRound = 3;
+				result.PlayerOutcome = Enumerations.Outcome.Tie;
+			}
+
+            #endregion
+
+            #region Calculate hand shape based points
+
+            switch (roundToPlay.OpponentHand)
+            {
+                case Enumerations.Hand.Rock:
+                    result.OpponentPointsThisRound += 1;
+                    break;
+                case Enumerations.Hand.Paper:
+                    result.OpponentPointsThisRound += 2;
+                    break;
+                case Enumerations.Hand.Scissor:
+                    result.OpponentPointsThisRound += 3;
+                    break;
+            }
+
+            switch (roundToPlay.PlayerHand)
+            {
+                case Enumerations.Hand.Rock:
+                    result.PlayerPointsThisRound += 1;
+                    break;
+                case Enumerations.Hand.Paper:
+                    result.PlayerPointsThisRound += 2;
+                    break;
+                case Enumerations.Hand.Scissor:
+                    result.PlayerPointsThisRound += 3;
+                    break;
+            }
+
+            #endregion
+
+            return result;
 		}
-
-
-
-
     }
 }
 
