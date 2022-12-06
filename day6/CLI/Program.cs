8df7af9c25a409037c71e153158f913a640824bc
Juan Martinez <jmartinez@flinks.com>
2022-12-06 08:09:55 -0500

    Day 6 - star 1 solution

--- day6/CLI/Program.cs
+++ day6/CLI/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Diagnostics;
+using System.IO;
+
+using AOC2022.Day6.TuningTrouble;
+
+string inputDataPath = AppDomain.CurrentDomain.BaseDirectory + "input-data.txt";
+List<string> inputData = FileLoader.LoadInput(inputDataPath);
+
+ElfDevice device = new ElfDevice(inputData[0]);
+
+// Works in terminal
+Console.WriteLine("Advent of code - Day 6 - console");
+Console.WriteLine("");
+Console.WriteLine($"First marker at: {device.FirstMarkerIndex}");
+Console.WriteLine("");
+
+System.Diagnostics.Debug.WriteLine("Advent of code - Day 6 - debug");
+System.Diagnostics.Debug.WriteLine("");
+System.Diagnostics.Debug.WriteLine($"First marker at: {device.FirstMarkerIndex}");
+System.Diagnostics.Debug.WriteLine("");
