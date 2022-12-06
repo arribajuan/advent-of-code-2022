7ef642045cb96f39f010b305097cebb953debf83
Juan Martinez <jmartinez@flinks.com>
2022-12-06 07:59:07 -0500

    Day 6 - implementation

--- day6/TuningTrouble/ElfDevice.cs
+++ day6/TuningTrouble/ElfDevice.cs
@@ -14,6 +14,34 @@ public class ElfDevice
 
     private void FindFirstMarkerIndex()
     {
+        this.FirstMarkerIndex = 0;
 
+        char c1 = ' ';
+        char c2 = ' ';
+        char c3 = ' ';
+        char c4 = ' ';
+
+        int counter = 1;
+        foreach (char c in this.DataStream)
+        {
+            c1 = c2;
+            c2 = c3;
+            c3 = c4;
+            c4 = c;
+
+            if (!c1.Equals(' ') &&
+                !c2.Equals(' ') &&
+                !c3.Equals(' ') &&
+                !c4.Equals(' ') &&
+                !c1.Equals(c2) && !c1.Equals(c3) && !c1.Equals(c4) &&
+                !c2.Equals(c3) && !c2.Equals(c4) &&
+                !c3.Equals(c4))
+            {
+                this.FirstMarkerIndex = counter;
+                break;
+            }
+
+            counter++;
+        }
     }
 }
